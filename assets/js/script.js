b334e927c4578f791f9a8f0d062025581a5afa5f
alsornak <al.sorna01@gmail.com>
2022-01-17 22:42:33 +1100

    added all files

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,36 +7,36 @@ var historyWindow = document.getElementById('history-content');
 
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault();  // prevents automatic refresh
-  var Destination = inputDest.value;
-  inputDest1.value = Destination;
-  var Destination1 = inputDest1.value;
-  inputDest1.style.display = "none";
+    event.preventDefault();  // prevents automatic refresh
+    var Destination = inputDest.value;
+    inputDest1.value = Destination;
+    var Destination1 = inputDest1.value;
+    inputDest1.style.display ="none";
 
-  console.log(Destination1);
-  var Origin = inputFrom.value;
-  var chosenDate = formDate.value
+    console.log(Destination1);
+    var Origin = inputFrom.value;
+    var chosenDate = formDate.value   
+    
+    
+    if (Destination1){
+      // Add function here to display map of destination
+      // Add function here to display weather at destination with or without dates
+      citySearch();
+      modal.style.display = 'block';
 
-
-  if (Destination1) {
-    // Add function here to display map of destination
-    // Add function here to display weather at destination with or without dates
-    citySearch();
-    modal.style.display = 'block';
-
-
-    saveToHistory(); // Function to save history
-
-
-  }
-
-  else if (Destination && Origin) {
-    // Add function here to display route from start location to destination
-    // Add function here to display weather at destination with or without dates
-
-    saveToHistory(); // Function to save history
-
-  }
+      
+      saveToHistory(); // Function to save history
 
 
-});
+    }
+
+    else if (Destination && Origin){
+      // Add function here to display route from start location to destination
+      // Add function here to display weather at destination with or without dates
+
+      saveToHistory(); // Function to save history
+
+    }
+   
+    
+  });
